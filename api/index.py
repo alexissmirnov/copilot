daa18b5c3249f81b1ee1e17868ee0913c7da8b13
Alexis Smirnov <alexis@smirnov.ca>
2023-06-30 20:12:09 +0000

    notion

--- api/index.py
+++ api/index.py
@@ -1,12 +1,17 @@
 ''' index '''
 from flask import Flask
 from langchain.llms import OpenAI
+from langchain.document_loaders import NotionDBLoader
+
 import os
 
-openai_api_key=os.getenv('OPENAI_API_KEY')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+NOTION_INTEGRATION_SECRET = os.getenv('NOTION_INTEGRATION_SECRET')
+NOTION_DB = os.getenv('NOTION_DB')
+
 app = Flask(__name__)
 
-llm = OpenAI(openai_api_key=openai_api_key)
+llm = OpenAI(openai_api_key=OPENAI_API_KEY)
 
 def sanitize_string(input_string):
     first_letter = input_string[0]
@@ -14,6 +19,7 @@ def sanitize_string(input_string):
     middle_length = len(input_string) - 2
     transformed_string = first_letter + '*' * middle_length + last_letter
     return transformed_string
+
 
 ''' home '''
 @app.route('/')
@@ -23,4 +29,15 @@ def home():
 ''' about '''
 @app.route('/about')
 def about():
-    return 'About'
+    return 'API key:' + sanitize_string(OPENAI_API_KEY)
+
+'''instructions'''
+@app.route('/instructions')
+def instructions():
+    loader = NotionDBLoader(
+        integration_token=NOTION_INTEGRATION_SECRET,
+        database_id=NOTION_DB,
+        request_timeout_sec=15,  # optional, defaults to 10
+    )
+    docs = loader.load()
+    return docs
