324138f3db5d0f1009090d9d4fd15a620145f868
Alexis Smirnov <alexis@smirnov.ca>
2023-06-30 19:22:25 +0000

    cleanup

--- api/index.py
+++ api/index.py
@@ -1,12 +1,14 @@
+''' index '''
 from flask import Flask
 
 app = Flask(__name__)
 
+''' home '''
 @app.route('/')
 def home():
     return 'Hello, World!'
 
+''' about '''
 @app.route('/about')
 def about():
     return 'About'
-    
