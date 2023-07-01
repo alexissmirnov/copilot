e83f1c916bcc8fc19290094059e8a5969e755972
Alexis Smirnov <alexis@smirnov.ca>
2023-07-01 16:48:28 +0000

    doc

--- api/index.py
+++ api/index.py
@@ -1,10 +1,8 @@
 ''' index '''
+import os
 from flask import Flask
 from langchain.llms import OpenAI
 from langchain.document_loaders import NotionDBLoader
-
-import os
-import json
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 NOTION_INTEGRATION_SECRET = os.getenv('NOTION_INTEGRATION_SECRET')
@@ -14,7 +12,9 @@ app = Flask(__name__)
 
 llm = OpenAI(openai_api_key=OPENAI_API_KEY)
 
+
 def sanitize_string(input_string):
+    """ to show the API key """
     first_letter = input_string[0]
     last_letter = input_string[-1]
     middle_length = len(input_string) - 2
@@ -22,35 +22,42 @@ def sanitize_string(input_string):
     return transformed_string
 
 
-''' home '''
 @app.route('/')
 def home():
+    """ home """
     return llm.predict("What would be a good company name for a company that makes colorful socks?")
 
-''' about '''
+
 @app.route('/about')
 def about():
-    return 'API key:' + sanitize_string(OPENAI_API_KEY)
+    """ about """
+    return f"API key: {sanitize_string(OPENAI_API_KEY)}\nNotion DB: {sanitize_string(NOTION_DB)}"
 
-### instructions
+# instructions
 # [
 #     Document(
-#         page_content='test of the first instruction', 
-#         metadata={'description': None, 'tags': [], 'name': 'first instruction', 'id': '99743a38-f434-4a2e-bc0a-6ecdcd0856b1'}), 
+#         page_content='test of the first instruction',
+#         metadata={'description': None, 'tags': [], 'name': 'first instruction', 'id': '99743a38-f434-4a2e-bc0a-6ecdcd0856b1'}),
 #     Document(
-#         page_content='', 
-#         metadata={'description': None, 'tags': [], 'name': 'third instruction', 'id': 'a8a1dd4b-9e69-4920-bfd2-9025095ccd44'}), 
+#         page_content='',
+#         metadata={'description': None, 'tags': [], 'name': 'third instruction', 'id': 'a8a1dd4b-9e69-4920-bfd2-9025095ccd44'}),
 #     Document(
-#         page_content='', 
+#         page_content='',
 #         metadata={'description': None, 'tags': [], 'name': 'second instruction', 'id': 'ebe2b3c6-a118-4eac-be20-d8c1bfea72a4'})
 # ]
+
+
 @app.route('/instructions')
 def instructions():
+    """ instrustions """
     loader = NotionDBLoader(
         integration_token=NOTION_INTEGRATION_SECRET,
         database_id=NOTION_DB,
         request_timeout_sec=15,  # optional, defaults to 10
     )
     docs = loader.load()
-    docs_string = f"docs: {docs}"
-    return json.dumps(docs_string, indent=4)
+    output = 'output: \n'
+    for doc in docs:
+        output = f"{output}\n# {doc.metadata['name']}\n{doc.page_content}"
+
+    return output
