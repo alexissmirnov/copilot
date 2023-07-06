1ae9fb4092b816a1118e3bbc84c73a78caf2f5a8
Keval Pipalia <keval.pipalia@dialogue.co>
2023-07-06 10:50:39 -0400

    Reduced temperature for more deterministic behaviour

--- api/index.py
+++ api/index.py
@@ -13,7 +13,7 @@ NOTION_EXAMPLES_DB = os.getenv('NOTION_EXAMPLES_DB')
 
 app = Flask(__name__)
 
-llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+llm = OpenAI(openai_api_key=OPENAI_API_KEY, temperature=0)
 
 
 def sanitize_string(input_string):
